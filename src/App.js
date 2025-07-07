0b7118f49ecc1d13c416c8fac8af9808dcbcfbcd
Ernesto <ernestosadeghi@gmail.com>
2025-07-07 18:04:10 +0330

    contact page was useless

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './layout/Header';
 import Home from './page/Home'
 import Breeds from './page/Breeds'
 
-import Contact from './page/Contact'
+
 import FunFact from './page/FunFact'
 import { Route, Routes   } from "react-router";
 function App() {
@@ -18,8 +18,6 @@ function App() {
       <Route index element={<Home/>}></Route>
       <Route path='breeds' element={<Breeds/>}></Route>
    
-      
-      <Route path='contact' element={<Contact/>}></Route>
       <Route path='fun' element={<FunFact/>}></Route>
       </Routes>
   
